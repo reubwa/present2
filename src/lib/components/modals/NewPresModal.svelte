9cb717da7906d8d1814f6e9aba76997d33e86140
Reuben Waring <reuben.waring@outlook.com>
2026-02-22 21:58:44 +0000

    Move button styling to global styles

--- src/lib/components/modals/NewPresModal.svelte
+++ src/lib/components/modals/NewPresModal.svelte
@@ -10,15 +10,6 @@
     <h1 class="text-white font-bold">New Presentation</h1>
     <form class="flex flex-row gap-2.5" onsubmit={(e) => show = false}>
         <input type="text" bind:value={result} placeholder="Presentation Name" class="text-[25px] ml-1.25 text-[lightblue]"/>
-        <button class="border-none rounded-[10px] text-white p-2.5" type="submit"><SquareArrowRightExitIcon/></button>
+        <button type="submit"><SquareArrowRightExitIcon/></button>
     </form>
 </BaseDialog>
-<style>
-    button{
-        background-color: var(46, 68, 68);
-    }
-    button:hover {
-        cursor: pointer;
-        background-color: rgb(66, 98, 98);
-    }
-</style>
