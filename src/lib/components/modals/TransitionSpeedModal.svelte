939ac3742c0eac927aef9a0a2cfd68965a1ec63b
Reuben Waring <reuben.waring@outlook.com>
2026-02-22 21:18:58 +0000

    TransitionSpeedModal

---
+++ src/lib/components/modals/TransitionSpeedModal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { CircleSlash2Icon, RabbitIcon, TurtleIcon } from "@lucide/svelte";
+	import BaseDialog from "./BaseDialog.svelte";
+	import GalleryButton from "./GalleryButton.svelte";
+    import {TransitionSpeeds} from "../../structs.ts";
+
+	let {show = $bindable(), result = $bindable()} = $props();
+	let dialog: HTMLDialogElement | undefined = $state();
+
+    function completeDialog(speed : TransitionSpeeds){
+        result = speed;
+        show = false;
+    }
+</script>
+<BaseDialog bind:dialog bind:show>
+    <h1 class="text-white font-bold">Transition Speed</h1>
+    <div class="flex flex-col gap-2.5">
+        <div class="flex flex-row gap-2.5">
+            <GalleryButton iconSize=32 icon={CircleSlash2Icon} caption="Default" clickEvent={()=>{completeDialog(TransitionSpeeds.Default)}}/>
+            <GalleryButton iconSize=32 icon={RabbitIcon} caption="Fast" clickEvent={()=>{completeDialog(TransitionSpeeds.Fast)}}/>
+            <GalleryButton iconSize=32 icon={TurtleIcon} caption="Slow" clickEvent={() => {completeDialog(TransitionSpeeds.Slow)}}/>
+        </div>
+    </div>
+</BaseDialog>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import AddSlideModal from "$lib/components/modals/AddSlideModal.svelte";
 	import ChangeThemeModal from "$lib/components/modals/ChangeThemeModal.svelte";
 	import TransitionModal from "$lib/components/modals/TransitionModal.svelte";
+	import TransitionSpeedModal from "$lib/components/modals/TransitionSpeedModal.svelte";
 	import Toolbar from "$lib/components/toolbar/Toolbar.svelte";
     import ToolbarButton from "$lib/components/toolbar/ToolbarButton.svelte";
 	import ToolbarGroup from "$lib/components/toolbar/ToolbarGroup.svelte";
@@ -18,6 +19,9 @@
     let transitionResponse = $state("");
 
     let transitionModal = $state();
+
+    let transitionSpeedModalVisibility = $state(false);
+    let transitionSpeedResponse = $state("");
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
@@ -37,18 +41,21 @@
             transitionModal = TransitionModes.Exit;
             transitionModalVisibility = true;
         }} iconSize="32"/>
-        <ToolbarButton caption="Test" icon={AccessibilityIcon} clickEvent={() => {}} iconSize="32"/>
+        <ToolbarButton caption="Speed" icon={AccessibilityIcon} clickEvent={() => {transitionSpeedModalVisibility=true}} iconSize="32"/>
     </ToolbarGroup>
 </Toolbar>
 
 <AddSlideModal bind:show={addSlideModalVisibility} bind:result={addSlideResponse}/>
 <ChangeThemeModal bind:show={changeThemeModalVisibility} bind:result={changeThemeResponse} />
 <TransitionModal bind:show={transitionModalVisibility} bind:result={transitionResponse} bind:mode={transitionModal}/>
+<TransitionSpeedModal bind:show={transitionSpeedModalVisibility} bind:result={transitionSpeedResponse} />
 
 <p>{addSlideResponse}</p>
 <p>{changeThemeResponse}</p>
 
 <p>{transitionModal} transition: {transitionResponse}</p>
+
+<p>{transitionSpeedResponse}</p>
 
 <svelte:head>
     <style>
