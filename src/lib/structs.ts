f8f43d6942526dd3691c4712c629535c1232a13e
Reuben Waring <reuben.waring@outlook.com>
2026-02-22 20:33:14 +0000

    Add other enums

--- src/lib/structs.ts
+++ src/lib/structs.ts
@@ -5,6 +5,39 @@ export const SlideTypes = {
     Image : "Image",
     Maths : "Maths",
     Code : "Code"
-} as const;
+};
+
+export const BuiltInThemes = {
+    Beige : "Beige",
+    Blood : "Blood",
+    Black : "Black",
+    League : "League",
+    Moon : "Moon",
+    Night : "Night",
+    Serif : "Serif",
+    Simple : "Simple",
+    Sky : "Sky",
+    Solarized : "Solarized",
+    White : "White",
+    Dracula : "Dracula"
+};
+
+export const Transitions = {
+    None : "None",
+    Fade : "Fade",
+    Slide : "Slide",
+    Convex : "Convex",
+    Concave : "Concave",
+    Zoom : "Zoom"
+};
+
+export const TransitionSpeeds = {
+    Default : "Default",
+    Fast : "Fast",
+    Slow : "Slow"
+}
 
 export type SlideTypes = typeof SlideTypes[keyof typeof SlideTypes];
+export type BuiltInThemes = typeof BuiltInThemes[keyof typeof BuiltInThemes];
+export type Transitions = typeof Transitions[keyof typeof Transitions];
+export type TransitionSpeeds = typeof TransitionSpeeds[keyof typeof TransitionSpeeds];
