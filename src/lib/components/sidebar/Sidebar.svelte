4ec8dec1c383d73a2334b5334bcfc5af9b307b1f
Reuben Waring <reuben.waring@outlook.com>
2026-02-23 20:53:13 +0000

    Add sidebar

---
+++ src/lib/components/sidebar/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let { children } = $props();
+</script>
+<div class="flex flex-col rounded-[10px] p-2.5 gap-2.5 text-white">
+    {@render children?.()}
+</div>
+<style>
+    div {
+        background-color: rgb(26, 42, 42);
+    }
+</style>
