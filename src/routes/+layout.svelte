63862d91342e75fd1baaa35f07f8f3ee54310f6e
Reuben Waring <reuben.waring@outlook.com>
2026-03-20 18:15:37 +0000

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { base } from '$app/paths';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head><link rel="icon" href="{base}/favicon.ico" /></svelte:head>
 {@render children()}
